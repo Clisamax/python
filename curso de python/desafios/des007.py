06ecfa52be3d2fa9570649dab26c724e467cf5c8
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-02-27 15:08:48 -0300

    des008

---
+++ curso de python/desafios/des007.py
@@ -0,0 +1,3 @@
+n1 = float(input('Digite quanto vc tem na carteira: '))
+s = float(n1 * 3.27)
+print('Vc tem na carteira {:.2f}, e pode comprar US${:.2f}'.format(n1, s))
---
+++ curso de python/desafios/des008.py
@@ -0,0 +1,8 @@
+from turtle import clear
+
+
+n1 = float(input('Digite a altura da parede: '))
+n2 = float(input('Digite a largura da parede: '))
+a = n1 * n2
+b = a / 2
+print('Sera necessario {} litros de tinta para pintar a parede.'.format(b))
