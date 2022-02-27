ab5276fc552e78ee61931a6ab91408d4df1554cb
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-02-27 01:03:14 -0300

    python ex05

---
+++ curso de python/ex001/ex005.py
@@ -0,0 +1,8 @@
+from unittest.mock import AsyncMagicMixin
+
+
+a = input('Digite Algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print ('Só tem espaços? ', a.isspace())
+print ('É alfabético? ', a.isalpha())
+print ('É alfanumérico? ', a.isalnum())
