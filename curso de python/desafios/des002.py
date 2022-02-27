edc306ea6833367d374ca654f080c2322725b619
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-02-27 14:06:50 -0300

    desafios

---
+++ curso de python/desafios/des002.py
@@ -0,0 +1,2 @@
+n1 = int(input('Digite um numero '))
+print('Seu antecessor é {}, Seu secussor é {}'.format(n1-1, n1+1))
---
+++ curso de python/desafios/des003.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um numero: '))
+print('Seu dobro é {}, seu tripo é {}, e a raiz é {}'.format(n1*2, n1*3, n1**(1/2)))
+
