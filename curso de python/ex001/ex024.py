27fb1cb3350d966e55ae7450512d6a7911743abe
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-03-08 15:30:13 -0300

    ex025

---
+++ curso de python/ex001/ex024.py
@@ -0,0 +1,3 @@
+# Verificando primeiras letras de um texto
+cid = str(input('Em que cidade você nasceu? ')).strip()
+print(cid[:5].upper() == 'SANTO') 
---
+++ curso de python/ex001/ex025.py
@@ -0,0 +1,3 @@
+# Procurando uma string dentro de outra
+nome = str(input('Digite seu nome Completo: ')).strip()
+print(' Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
