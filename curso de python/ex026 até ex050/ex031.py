92c072259dbf789dc6a4420268f912f5c22c12ca
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-03-11 20:24:52 -0300

    Create ex031.py

---
+++ curso de python/ex026 até ex050/ex031.py
@@ -0,0 +1,7 @@
+dist = float(input('Qual a distancia da viagem? '))
+if dist <= 200:
+    pas = dist * 0.50
+    print('O valor da passagem sera de R${:.2f}'.format(pas))
+else:
+    pas = dist * 0.45
+    print('O valor da passagem sera de R${:.2f}'.format(pas))
