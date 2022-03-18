5ce996f61b90160dff630d07a0eba74cd1ee42dd
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-03-17 21:53:43 -0300

    creat ex045

--- curso de python/ex026 até ex050/ex044.py
+++ curso de python/ex026 até ex050/ex044.py
@@ -1,3 +1,5 @@
+
+print('{:=^40}'.format('LOJAS MKAGOMES'))  
 preço = float(input('Preço das compras: R$'))
 print(''' Formas de pagamentos 
 [1] à vista dinheiro/ cheque 
@@ -8,11 +10,17 @@ opção = int(input('Qual é a opção: '))
 if opção == 1:
     total = preço - (preço * 10 / 100)
 elif opção == 2:
-     total = preço + (preço * 5 / 100)
+    total = preço + (preço * 5 / 100)
 elif opção == 3:
+    parcela = preço / 2
     total = preço 
+    print('Sua compra será parcelada em 2x de {:.2f}'.format(parcela))
 elif opção == 4:
-    total = preço + (preço * 20 / 100)
-     
-print('{:=^40}'.format('LOJAS MKAGOMES'))    
-print(total)
+    total = preço + (preço * 30 / 100)
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de {:.2f}'.format(totparc, parcela))
+else:
+    total = preço 
+    print('OPÇÃO INVÁLIDA, TENTE NOVAMENTE!')
+print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, total))  
