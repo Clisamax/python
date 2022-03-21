20c8b2de9aa15e6650fc7d12913bc179b556677e
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-03-21 12:34:15 -0300

    create ex051

---
+++ curso de python/aulas/função.py
@@ -0,0 +1,15 @@
+# função
+altura = 1080
+largura = 1920
+area = largura * altura
+print(area)
+
+#função calcula area retangulo
+#def nome_função():
+def area_retangulo(largura,altura):
+    area = largura * altura
+    return area
+
+print(area_retangulo(4,3))
+
+
