9ba8090f2089b7edd374fb561ecc483e4f88a72a
clisamax <clisamax.gomes.dev@gmail.com>
2022-04-06 17:54:05 -0300

    ex070

--- curso de python/ex051 até ex075/ex070.py
+++ curso de python/ex051 até ex075/ex070.py
@@ -1,12 +1,23 @@
 print('-' * 20)
 print('LOJAS MKAGOMES')
 print('-' * 20)
-resp = prod = ' '
-prec = tot = prodMB = 0
+total = preço = totMil = menor = cont = 0 
+barato = ' '
 while True:
     prod = str(input('Nome do produto: '))    
-    prec = float(input('Preço: '))
+    preço = float(input('Preço: '))
+    cont += 1
+    total += preço
+    resp = prod = ' '
+    if preço > 1000:
+        totMil += 1
+    if cont == 1 or preço < menor:
+        menor = preço
+        barato = prod
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
+print(f'O total da compra foi R${total:.2f}')
+print(f' Temos {totMil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato custa R${menor:.2f}')
